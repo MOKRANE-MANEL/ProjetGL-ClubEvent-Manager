7ecca419042820b0f472d0975307238685b8b853
Mohgithup <mohamedhamm284@gmail.com>
2025-11-27 20:52:30 +0100

    Create AdminObserver.java

---
+++ src/model/AdminObserver.java
@@ -0,0 +1,15 @@
+public class AdminObserver implements Observeur {
+
+    private String adminName;
+
+    public AdminObserver(String adminName) {
+        this.adminName = adminName;
+    }
+
+    @Override
+    public void update(Event event) {
+        System.out.println("ADMIN " + adminName +
+                " A ÉTÉ NOTIFIÉ : Nouvelle inscription dans l'événement → "
+                + event.getName());
+    }
+}
